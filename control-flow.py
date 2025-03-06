ded1ab04473ce71c9221ebfd3b55494a72750ce2
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2025-03-06 11:19:28 +0530

    conditional statements

---
+++ control-flow.py
@@ -0,0 +1,12 @@
+marks = 85
+
+if marks >=90:
+    print("Grade: A")
+elif marks >=75:
+    print("Grade: B")
+else:
+    print("Grade: C")
+
+#shorthand 
+x = 7
+print("Even") if x % 2 == 0 else print("Odd")
