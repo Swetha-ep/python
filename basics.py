e56cc311aa16f2ac568ee0aaaab01965dbcea40e
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2025-03-06 10:50:31 +0530

    datatypes and type casting/conversion

---
+++ basics.py
@@ -0,0 +1,20 @@
+#data types:
+#int
+#float
+#str
+#bool
+#list
+#tuple
+#dict
+#set
+#---------------------------------------
+
+#type casting
+x = '25'
+y = int(x) + 5
+print(y,type(y))
+
+num_list = [1,2,3,4]
+num_tuple = tuple(num_list)
+print(num_tuple,type(num_tuple))
+
